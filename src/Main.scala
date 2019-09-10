688a24dcc7939e4534b8bcaceb5bb6045d045e56
Félix Pelletier <felixpelletier23@gmail.com>
2019-09-09 23:11:49 -0400

    Cleanup and added diffuse

--- src/Main.scala
+++ src/Main.scala
@@ -4,8 +4,6 @@ import java.awt.{Color, Image}
 import javax.swing.{ImageIcon, JFrame, JLabel, WindowConstants}
 import java.awt.FlowLayout
 
-import Main.getClosestIntersection
-
 import scala.language.reflectiveCalls
 
 object Main extends App {
@@ -35,11 +33,10 @@ object Main extends App {
       case multiplier: FloatColor => FloatColor(r * multiplier.r, g * multiplier.g, b * multiplier.b)
       case multiplier: Float => FloatColor(r * multiplier, g * multiplier, b * multiplier)
     }
-    def scale(scalar : Float) = FloatColor(r * scalar, g * scalar, b * scalar)
   }
 
   case class Scene(solids : List[SolidObject], lights : List[Light]) {
-    def cameraPosition : Vector3D = Vector3D(0, 0, 0)
+    def cameraPosition = Vector3D(0, 0, 0)
   }
 
   def constraint(value: Int, min: Int, max: Int) : Int = {
@@ -58,33 +55,23 @@ object Main extends App {
   )
 
   case class Vector2D(x : Float, y : Float){
-    def +(otherVector: Vector2D) : Vector2D = Vector2D(x + otherVector.x, y + otherVector.y)
-    def -(otherVector: Vector2D) : Vector2D = Vector2D(x - otherVector.x, y - otherVector.y)
+    def +(otherVector: Vector2D) = Vector2D(x + otherVector.x, y + otherVector.y)
+    def -(otherVector: Vector2D) = Vector2D(x - otherVector.x, y - otherVector.y)
   }
 
   case class Vector3D(x : Float, y : Float, z : Float) {
 
     def +(otherVector: Vector3D) = Vector3D(x + otherVector.x, y + otherVector.y, z + otherVector.z)
-
     def -(otherVector: Vector3D) = Vector3D(x - otherVector.x, y - otherVector.y, z - otherVector.z)
-
     def *(scalar: Float) = Vector3D(scalar * x, scalar * y, scalar * z)
 
-    def length2() : Float = {
-      x*x + y*y + z*z
-    }
-
-    def length() : Float = {
-      math.sqrt(length2()).toFloat
-    }
-
-    def dot(otherVector : Vector3D) : Float = {
-      x * otherVector.x + y * otherVector.y + z * otherVector.z
-    }
+    def length2(): Float = x*x + y*y + z*z
+    def length(): Float = math.sqrt(length2()).toFloat
+    def dot(otherVector : Vector3D): Float = x * otherVector.x + y * otherVector.y + z * otherVector.z
 
     def normalize : Vector3D = {
       val l = length()
-      Vector3D(x / l, y / l, z/l)
+      Vector3D(x / l, y / l, z / l)
     }
 
   }
@@ -92,16 +79,16 @@ object Main extends App {
   case class Ray(origin : Vector3D, direction : Vector3D, length : Float)
 
   trait SolidObject {
-    def IntersectRay(origin: Vector3D, direction: Vector3D): Option[Vector3D]
+    def IntersectRay(origin: Vector3D, direction: Vector3D): Option[(Vector3D, Vector3D)]
     def color : FloatColor
   }
 
   case class Floor(height : Float, color : FloatColor) extends SolidObject {
-    def IntersectRay(origin: Vector3D, direction: Vector3D): Option[Vector3D] = {
+    def IntersectRay(origin: Vector3D, direction: Vector3D): Option[(Vector3D, Vector3D)] = {
       val normal = Vector3D(0.0f, 1.0f, 0.0f)
       val planeDistance = (Vector3D(0.0f, height, 0.0f) - origin).dot(normal) / direction.dot(normal)
       if (planeDistance >= 0.0f){
-        Some(origin + direction * planeDistance)
+        Some((origin + direction * planeDistance, normal))
       }
       else{
         None
@@ -110,67 +97,66 @@ object Main extends App {
   }
 
   case class Sphere(position : Vector3D, radius : Float, color : FloatColor) extends SolidObject {
-
-    def IntersectRay(origin: Vector3D, direction: Vector3D): Option[Vector3D] = {
+    def IntersectRay(origin: Vector3D, direction: Vector3D): Option[(Vector3D, Vector3D)] = {
       val squaredHalfIntersectionLength = math.pow(direction.dot(origin - position), 2.0) - ((origin - position).length2() - (radius*radius))
       if (squaredHalfIntersectionLength < 0.0f) return None
 
-      val halfIntersectionLength = math.sqrt(squaredHalfIntersectionLength)
-      val centerIntersectionDistance = 0 - direction.dot(origin - position)
+      val halfIntersectionLength = math.sqrt(squaredHalfIntersectionLength).toFloat
+      val centerIntersectionDistance = 0.0f - direction.dot(origin - position)
       val closestIntersectionDistance = centerIntersectionDistance - halfIntersectionLength
 
-      Some(origin + direction * closestIntersectionDistance.toFloat)
+      val intersectionPoint = origin + direction * closestIntersectionDistance.toFloat
+      val normal = (intersectionPoint - position).normalize
+
+      Some(intersectionPoint, normal)
     }
   }
 
   trait Light {
-    def CalculateLighting(scene : Scene, point : Vector3D) : FloatColor
+    def CalculateLighting(scene : Scene, point : Vector3D, normal : Vector3D) : FloatColor
   }
 
   case class AmbientLight(color : FloatColor) extends Light {
-    def CalculateLighting(scene : Scene, point : Vector3D) = color
+    def CalculateLighting(scene : Scene, point : Vector3D, normal : Vector3D): FloatColor = color
   }
 
   case class PointLight(position : Vector3D, color : FloatColor) extends Light {
-    def CalculateLighting(scene : Scene, point : Vector3D): FloatColor = {
+    def CalculateLighting(scene : Scene, point : Vector3D, normal : Vector3D): FloatColor = {
 
       val direction = (point - position).normalize
       val closestObject = getClosestIntersection(position, direction)
       if (closestObject.isDefined) {
-        if ((point - closestObject.get._2).length() < 0.001F) {
+        if ((point - closestObject.get._2).length2() < 0.000001F) {
           val distance_squared = (position - point).length2()
           val lighting = 1.0f / distance_squared
-          color * lighting
-        }
-        else {
-          FloatColor(0.0f, 0.0f, 0.0f)
+          val angleAttenuation = -(direction.dot(normal))
+          return color * lighting * angleAttenuation
         }
       }
-      else {
-        FloatColor(0.0f, 0.0f, 0.0f)
-      }
+
+      FloatColor(0.0f, 0.0f, 0.0f)
     }
   }
 
   def generateScreenUVs(width: Int, height: Int, fov: Float): Iterable[((Int, Int), Vector3D)] = {
-    val screenDistance = math.tan(FOV / 2).floatValue
-    val topScreenHeight = HEIGHT.toFloat / WIDTH.toFloat
+    val screenDistance = 1.0f / math.tan(math.toRadians(fov) / 2.0f).floatValue
+    val topScreenHeight = height.toFloat / width.toFloat
 
     for (x <- 0 until width; y <- 0 until height) yield {
       val u = (x / (width / 2.0f)) - 1.0f
-      val v = topScreenHeight * (((height/2.0f) - y) / (height / 2.0f)) // TODO: Simplify this
+      val v = topScreenHeight * (1.0f - (y / (height / 2.0f)))
       ((x, y), Vector3D(u, v, screenDistance))
     }
   }
 
-  def getClosestIntersection(origin : Vector3D, direction : Vector3D): Option[(SolidObject, Vector3D)] ={
+  def getClosestIntersection(origin : Vector3D, direction : Vector3D): Option[(SolidObject, Vector3D, Vector3D)] ={
     scene.solids.map{case (obj : SolidObject) => (obj, obj.IntersectRay(origin, direction)) }
       .filter(_._2.isDefined)
       .map {
-        case (solid: SolidObject, intersection : Some[Vector3D]) => (solid, intersection.get)
+        case (solid: SolidObject, intersection : Some[(Vector3D, Vector3D)]) => (solid, intersection.get._1, intersection.get._2)
       }
       .minByOption {
-        case (_: SolidObject, intersection : Vector3D) => {
+        case (_: SolidObject, intersection : Vector3D, _ : Vector3D) => {
           (intersection - origin).length2()
         }
       }
@@ -181,7 +167,8 @@ object Main extends App {
     val closestObject = getClosestIntersection(origin, direction)
     if (closestObject.isDefined) {
       val intersectionPoint = closestObject.get._2
-      val lighting = scene.lights.map {_.CalculateLighting(scene, intersectionPoint)}
+      val normal = closestObject.get._3
+      val lighting = scene.lights.map {_.CalculateLighting(scene, intersectionPoint, normal)}
         .fold(FloatColor(0,0,0)){_ + _}
 
       val objectColor = closestObject.get._1.color
@@ -203,27 +190,44 @@ object Main extends App {
   val scene = Scene(
     solids = List(
       Floor(-2.0f, FloatColor(0.25f, 0.25f, 0.25f)),
-      Sphere(Vector3D(0, 0, 5.0f), 1.0f, FloatColor(1.0f, 0.0f, 0.0f)),
-      Sphere(Vector3D(2.5f, 0.0f, 7.5f), 1.0f, FloatColor(0.0f, 1.0f, 0.0f)),
-      Sphere(Vector3D(-2.5f, -1.0f, 10.0f), 1.0f, FloatColor(1.0f, 0.1f, 1.0f)),
-      Sphere(Vector3D(0.1f, 1.0f, 7.5f), 1.0f, FloatColor(0.5f, 0.1f, 1.0f)),
-      Sphere(Vector3D(1.0f, -1.0f, 9.0f), 1.0f, FloatColor(0.0f, 1.0f, 1.0f)),
-      Sphere(Vector3D(-5.0f, 1.0f, 15.0f), 1.0f, FloatColor(1.0f, 1.0f, 1.0f)),
+      Sphere(Vector3D(-0.35f, 0.20f, 4.0f), 1.0f, FloatColor(1.0f, 0.0f, 0.0f)),
+      Sphere(Vector3D(3.5f, 1.5f, 5.5f), 1.0f, FloatColor(0.0f, 1.0f, 0.0f)),
+      Sphere(Vector3D(-4.5f, -1.0f, 8.0f), 1.0f, FloatColor(1.0f, 0.1f, 1.0f)),
+      Sphere(Vector3D(1.0f, 2.0f, 9.0f), 1.0f, FloatColor(0.5f, 0.1f, 1.0f)),
+      Sphere(Vector3D(2.0f, -1.0f, 7.0f), 1.0f, FloatColor(0.0f, 1.0f, 1.0f)),
+      Sphere(Vector3D(-5.0f, 2.0f, 12.0f), 1.0f, FloatColor(1.0f, 1.0f, 1.0f)),
+      Sphere(Vector3D(5.0f, 1.0f, 12.0f), 1.0f, FloatColor(1.0f, 0.65f, 0.0f)),
     ),
     lights = List(
-      AmbientLight(FloatColor(0.25f, 0.25f, 0.25f)),
-      PointLight(Vector3D(-2.5f, 1.5f, 4.0f), FloatColor(10.0f, 10.0f, 10.0f))
+      AmbientLight(FloatColor(0.3f, 0.3f, 0.3f)),
+      PointLight(Vector3D(-3.5f, 2.5f, 2.0f), FloatColor(20.0f, 20.0f, 20.0f)),
     )
   )
+
+  def time[R](dryrun: Int, runs: Int, block: => R): R = {
+    for(_ <- 0 until dryrun) {
+      block
+    }
+    val t0 = System.nanoTime()
+    for(_ <- 1 until runs) {
+      block
+    }
+    val result = block    // call-by-name
+    val t1 = System.nanoTime()
+    val diff_ns = (t1 - t0) / runs
+    val diff_ms = diff_ns / 1000000.0
+    println("Elapsed time: " + diff_ms + " ms")
+    result
+  }
 
   generateScreenUVs(WIDTH, HEIGHT, FOV)
     .map {
       case (screenCoordinates: (Int, Int), uv: Vector3D) =>
         (screenCoordinates, getPixelColor(scene, uv))
     }
-    .map {
+    .foreach {
       case (screenCoordinates: (Int, Int), color: Some[FloatColor]) =>
-      rgbBitmap.update(screenCoordinates._1, screenCoordinates._2, floatColorToColor(color.value))
+        rgbBitmap.update(screenCoordinates._1, screenCoordinates._2, floatColorToColor(color.value))
       case (_: (Int, Int), None) => ()
     }
 
